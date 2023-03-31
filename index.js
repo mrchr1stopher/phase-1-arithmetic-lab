5b0c783b4a89c861ce8d5da8ce3deadba79121f5
Christopher Wiggins <cwiggins610@gmail.com>
2023-03-30 21:40:44 -0400

    lab compltete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.round(Math.random() * 10 + 1);
+
+  let num3 = 2;
+  let num4 = 2;
+  let mod = num3 + num4;
+
+let max = Math.max(1,8,17,20);
